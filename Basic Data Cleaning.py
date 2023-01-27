2822bc4f873c1e590d6c15c20d6fd474726a1144
Peter Canon <122401713+PeterCanon@users.noreply.github.com>
2023-01-27 10:18:23 -0500

    Updating .py cleaning method
    In addition to the basic cleaning process, this code takes a few extra steps to ensure that the data is as clean as possible. First, I use the Interquartile range method to remove any outliers. Then I check for any remaining missing data and fill it with the mean value. After that, I check for duplicate rows that may have been missed during the initial cleaning process and remove them. Keep in mind, this is an extensive example and may not be suitable for all data sets.

--- Basic Data Cleaning.py
+++ Basic Data Cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # Read the Excel file into a DataFrame
 df = pd.read_excel('data.xlsx')
@@ -30,11 +31,26 @@ for col in df.columns:
 # Check for outliers
 df.describe()
 
-# Remove outliers
-df = df[df['column_name'] < upper_bound]
+# Remove outliers using the Interquartile range method
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+IQR = Q3 - Q1
+df = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+# Check for any remaining missing data
+print(df.isna().sum())
+
+# Fill missing data with mean value
+df.fillna(df.mean(), inplace=True)
+
+# Check for duplicate rows after cleaning
+print(df.duplicated().sum())
+
+# Remove duplicate rows
+df.drop_duplicates(inplace=True)
 
 # Standardize data
-df['column_name'] = (df['column_name'] - df['column_name'].mean()) / df['column_name'].std()
+df = (df - df.mean()) / df.std()
 
 # Save the cleaned data to a new Excel file
 df.to_excel('cleaned_data.xlsx', index=False)
